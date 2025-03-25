24ecb4d0fbdfd9da3c398faba7fd22d7b5c89fa1
ssd-linux <giathinhnguyen2000@gmail.com>
2025-03-25 22:10:07 +0700

    Rebase USV firmware to V4.6.0-beta4

--- Rover/version.h
+++ Rover/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduRover V4.6.0-beta4"
+#define THISFIRMWARE "USVU1 V4.6.0 - beta4"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_BETA+3
